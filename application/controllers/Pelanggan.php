43ea878e36a0320a3e2df0874eb49072e5ae9a00
Khilda Maililhaq <alil@pop-os.localdomain>
2019-05-29 13:00:45 +0700

    ketiga

---
+++ application/controllers/Pelanggan.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class Pelanggan extends CI_Controller
+{
+    function index(){
+        $this->load->view("dashboardView");
+    }
+}
--- application/views/_partial/breadcrump.php
+++ application/views/_partial/breadcrump.php
@@ -5,9 +5,8 @@
             Data Tables
         </h1>
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="breadcrumb-item"><a href="#">Tables</a></li>
-            <li class="breadcrumb-item active">Data tables</li>
+            <li class="breadcrumb-item"><a href="#"><i class="fa fa-dashboard"></i> Master Pelanggan</a></li>
+
         </ol>
     </section>
     <?php $this->load->view("_partial/board")?>
